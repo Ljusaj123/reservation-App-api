46fd4dbe6ded06f40d5cf76346a1e584ec3c38f1
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-27 13:29:55 +0200

    connection to the db, created server

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app=express();
+const app = express();
+
+dotenv.config();
 const port = 5000;
 
-app.get("/", (req,res)=> res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello world"));
 
+const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.listen(port,()=>console.log(`Server is listening on the port ${port}`))
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected");
+});
+
+app.listen(port, () => {
+  connect(), console.log(`Server is listening on the port ${port}`);
+});
