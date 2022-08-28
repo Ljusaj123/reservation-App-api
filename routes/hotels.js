19e70e9aa215125bd063b87ed366c5c1da7ca693
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-28 13:24:14 +0200

    cleaned code

--- routes/hotels.js
+++ routes/hotels.js
@@ -9,12 +9,6 @@ import {
 
 const router = express.Router();
 
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
-router.get("/:id");
-router.get("/");
-
 router.route("/").post(createHotel).get(getAllHotels);
 router.route("/:id").put(updateHotel).delete(deleteHotel).get(getHotel);
 
