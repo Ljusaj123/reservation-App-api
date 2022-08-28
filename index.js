97d4021e9e51d00f6b98bad7e782537f3a31a77f
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-28 13:22:00 +0200

    uri paths changed, middleware for errors created and function to listen port and connect to the database

--- index.js
+++ index.js
@@ -5,51 +5,42 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import connectDB from "./database/connect.js";
 
 const app = express();
 
 dotenv.config();
-const port = 5000;
-
-app.get("/", (req, res) => res.send("Hello world"));
-
-const connect = async () => {
-  try {
-    mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to mongoDB");
-  } catch (error) {
-    throw error;
-  }
-};
-
-mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected");
-});
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
-});
 
 //middlewares
 app.use(express.json());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/hotels", hotelsRoute);
-app.use("/api/rooms", roomsRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/hotels", hotelsRoute);
+app.use("/api/v1/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong!";
-  return res
-    .status(errorStatus)
-    .json({
-      success: false,
-      status: errorStatus,
-      message: errorMessage,
-      stack: err.stack,
-    });
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
 });
 
-app.listen(port, () => {
-  connect(), console.log(`Server is listening on the port ${port}`);
-});
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  app.listen(port, () => {
+    try {
+      connectDB(process.env.MONGO_URI);
+      console.log(`Server is listening on the port ${port}`);
+    } catch (error) {
+      console.log("Failed to connect to MongoDB", error);
+    }
+  });
+};
+
+start();
