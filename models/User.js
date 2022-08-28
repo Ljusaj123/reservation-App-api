bea55ca2260054fa5a0824247807081814f49276
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-28 14:35:31 +0200

    created schema for user

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAdmin: {
+    type: boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
