691c3e844eee32c5cdaf401dc49be4a089e0c686
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-29 16:39:54 +0200

    called controller for authenticate admin

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,11 @@ import {
   getUser,
   updateUser,
 } from "../controllers/users.js";
-import { VerifyToken, VerifyUser } from "../middlewares/VerifyToken.js";
+import {
+  VerifyAdmin,
+  VerifyToken,
+  VerifyUser,
+} from "../middlewares/VerifyToken.js";
 
 const router = express.Router();
 
@@ -14,6 +18,9 @@ router.get("/checkauthentication", VerifyToken, (req, res, next) => {
 });
 router.get("/checkuser/:id", VerifyUser, (req, res, next) => {
   res.send("Hello user, you are logged in and can delete your account");
+});
+router.get("/checkadmin/:id", VerifyAdmin, (req, res, next) => {
+  res.send("Hello admin, you are logged in and can delete all accounts");
 });
 
 router.route("/").get(getAllUsers);
