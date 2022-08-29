21f3284e0be001677e9f43fc7eb55f921d92883b
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-29 02:10:15 +0200

    controllers for user

---
+++ controllers/user.js
@@ -0,0 +1,66 @@
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+
+export const updateUser = async (req, res, next) => {
+  const { id: userID } = req.params;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      const error = new Error();
+      error.status = StatusCodes.BAD_REQUEST;
+      error.message = `The user with id ${userID} does not exist`;
+      throw error;
+    }
+    res.status(StatusCodes.OK).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  const { id: userID } = req.params;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      const error = new Error();
+      error.status = StatusCodes.BAD_REQUEST;
+      error.message = `The user with id ${userID} does not exist`;
+      throw error;
+    }
+    res.status(StatusCodes.OK).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  const { id: userID } = req.params;
+  try {
+    const user = await User.findById(userID);
+    if (!user) {
+      const error = new Error();
+      error.status = StatusCodes.BAD_REQUEST;
+      error.message = `The user with id ${userID} does not exist`;
+      throw error;
+    }
+    res.status(StatusCodes.OK).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const getUsers = await User.find({});
+    res.status(StatusCodes.OK).json(getUsers);
+  } catch (error) {
+    next(error);
+  }
+};
