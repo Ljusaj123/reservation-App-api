8d7254b2c79ecff4c653fb27cd5a19703e121e8a
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-08-29 23:58:11 +0200

    added header to enable cors calls

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const app = express();
 dotenv.config();
 
 //middlewares
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
