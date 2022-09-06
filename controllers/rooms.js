4569d01a48150459af48eb61e59003909e5f829d
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-06 18:04:39 +0200

    function to check room availability, need update

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -36,6 +36,21 @@ export const updateRoom = async (req, res, next) => {
     next(error);
   }
 };
+// export const updateRoomAvailability = async (req, res, next) => {
+//   try {
+//     await Room.updateOne(
+//       { "roomNumbers._id": req.params.id },
+//       {
+//         $push: {
+//           "roomNumbers.$.unavailableDates": req.body.dates,
+//         },
+//       }
+//     );
+//     res.status(200).json("Room status has been updated.");
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const deleteRoom = async (req, res, next) => {
   const hotelID = req.params.hotelid;
