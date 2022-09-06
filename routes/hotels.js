4b515846f145ba81e31f2a19be456778063bd547
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-06 18:05:36 +0200

    added rotes to the new controllers

--- routes/hotels.js
+++ routes/hotels.js
@@ -7,12 +7,13 @@ import {
   getAllHotels,
   getHotel,
   updateHotel,
+  getHotelRooms,
 } from "../controllers/hotels.js";
 import { VerifyAdmin, VerifyUser } from "../middlewares/Verify.js";
 
 const router = express.Router();
 
-router.route("/").post(VerifyUser, createHotel).get(VerifyAdmin, getAllHotels);
+router.route("/").post(VerifyUser, createHotel).get(VerifyUser, getAllHotels);
 router
   .route("/:id")
   .patch(VerifyUser, updateHotel)
@@ -21,4 +22,5 @@ router
 
 router.route("/count/city").get(countByCity);
 router.route("/count/type").get(countByType);
+router.route("/room/:id").get(getHotelRooms);
 export default router;
